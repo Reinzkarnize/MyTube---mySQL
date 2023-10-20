933371ae6585874fc03f8cadea9959b9c739161f
Fauzi kurniawan <ozilpym123@gmail.com>
2023-10-20 13:40:02 +0700

    rev2

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from cs50 import SQL
 from pytube import Playlist, YouTube
 import os
 import mysql.connector
